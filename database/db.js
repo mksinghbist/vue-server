9ac530421057e7e16eaea34f7e71a5754df2063f
manoj kumar singh <mksinghbist@gmail.com>
2024-03-20 18:52:35 +0530

    db fixed issue

--- database/db.js
+++ database/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DB_CONNECTION_STRING,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
- .then(()=>console.log("Database  Connected"))
- .catch((error)=> console.log(error));
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.error(error));
 
- module.exports = mongoose;
+module.exports = mongoose;
